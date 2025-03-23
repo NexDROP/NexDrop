a3f20dce46f41465820f2e36aec3d584c9aaf4a1
NexDROP <dakxofficial5@gmail.com>
2025-03-23 19:48:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+          <script>
+            window.TelegramAdsController = new TelegramAdsController();
+            window.TelegramAdsController.initialize({
+              pubId: "954410",
+              appId: "1856",
+            });
+          </script> 
+              
 const airdropsData = [
     {
         id: 1,
